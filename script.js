20601d3b9b2360bb6c0392a88a32b51829cc8946
NinadxBaruah <117712522+NinadxBaruah@users.noreply.github.com>
2023-03-16 20:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const ageInput = document.getElementById("age");
+const inputName = document.getElementById("name");
+const btn = document.getElementById("btn");
+const form = document.querySelector("form");
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const age = parseInt(ageInput.value);
+  const namee = inputName.value;
+  if (!age || !namee) {
+    alert("Please fill the both fields");
+    return;
+  }
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve({ namee });
+      } else {
+        reject({ namee });
+      }
+    }, 4000);
+  });
+
+  myPromise
+    .then(({ namee }) => alert(`Welcome, ${namee}. You can vote.`))
+    .catch(({ namee }) => alert(`Oh sorry ${namee}. You aren't old enough.`));
+}
+form.addEventListener("submit", handleFormSubmit);
